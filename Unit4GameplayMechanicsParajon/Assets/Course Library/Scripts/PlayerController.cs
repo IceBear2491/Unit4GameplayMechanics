8fd3e9cdbaa2c15eb211a44cc186ef005dbd38c5
IceBear2491 <salvadorparajon5@gmail.com>
2026-04-21 15:25:06 -0700

    made it to step 13
    Need to fix it or something idk

--- Unit4GameplayMechanicsParajon/Assets/Course Library/Scripts/PlayerController.cs
+++ Unit4GameplayMechanicsParajon/Assets/Course Library/Scripts/PlayerController.cs
@@ -6,10 +6,14 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     private GameObject focalPoint;
     private float powerUpStrength = 15.0f;
+    private GameObject tmpRocket;
+    private Coroutine powerupCountdown;
 
     public bool hasPowerUp;
     public float speed = 5.0f;
     public GameObject powerUpIndicator;
+    public PowerUpType currentPowerUp = PowerUpType.None;
+    public GameObject rocketPrefab;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -23,6 +27,11 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+
+        if (currentPowerUp == PowerUpType.Rockets && Input.GetKeyDown(KeyCode.F))
+        {
+            LaucnhRockets();        
+        }
         powerUpIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
     }
 
@@ -31,25 +40,40 @@ public class PlayerController : MonoBehaviour
         if (other.CompareTag("PowerUp"))
         { 
             hasPowerUp = true;
+            currentPowerUp = other.gameObject.GetComponent<PowerUp>().powerUpType;
             Destroy(other.gameObject);
-            StartCoroutine(PowerUpCountdownRoutine());
-            powerUpIndicator.gameObject.SetActive(true);
+
+            if (powerupCountdown != null)
+            { 
+                StopCorountine(powerupCountdown);
+            }
+            powerupCountdown = StartCoroutine(PowerupCountdownRoutine());
+
         }
     }
     IEnumerator PowerUpCountdownRoutine()
     {
         yield return new WaitForSeconds(7);
         hasPowerUp = false;
+        currentPowerUp = PowerUpType.None;
         powerUpIndicator.gameObject.SetActive(false);
     }
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Enemy") && hasPowerUp)
+        if (collision.gameObject.CompareTag("Enemy") && currentPowerUp == PowerUpType.Pushback)
         {
             Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
             Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
-            Debug.Log("Collided with " + collision.gameObject.name + " with powerup set to " + hasPowerUp);
             enemyRigidbody.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Impulse);
+            Debug.Log("Player collided with: " + collision.gameObject.name + " with powerup set to " + currentPowerUp.ToString());
+        }
+    }
+    void LaunchRockets()
+    {
+        foreach (var enemy in FindObjectsOfType<Enemy>())
+        {
+            tmpRocket = Instantiate(rocketPrefab, transform.position + Vector3.up, Quaternion.identity);
+            tmpRocket.GetComponent<RocketBehaviour>().Fire(enemy.transform);
         }
     }
 }
